330f4d460606b5af1d1385b3ea2cda33a4ccf613
Caitlin Floyd <caitlinfloyd+coding@gmail.com>
2020-11-20 18:03:31 -0500

    feat: improve responsiveness of video display

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 // import PropTypes from "prop-types";
+import "../style/core.scss"
 
-import Image from "components/Image";
+// import Image from "components/Image";
 
 const Client = () => {
-  const imgPart = (
-   <iframe 
-   style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '50vh'}}
-    width="560" 
-    height="315" 
-    src="https://www.youtube.com/embed/S1IPBRd84ws" 
-    title="ceremony" 
-    frameBorder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-    allowFullScreen />
+  const liveStream = (
+   <div
+    className="videoWrapper"
+    // paddingBottom="50%"
+    // width="100%"
+   >
+    <iframe 
+      // style= {{display: 'flex',  justifyContent:'center', alignItems:'center', height: '50vh'}}
+      // width="100%" 
+      width="560" 
+      // height="100%" 
+      height="349" 
+      // position="absolute"
+      src="https://www.youtube.com/embed/S1IPBRd84ws" 
+      title="ceremony" 
+      frameBorder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+      allowFullScreen
+      />
+  </div>
   );
 
-  return imgPart;
+  return liveStream;
 };
 
 // Client.propTypes = {
