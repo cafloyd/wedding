3ae80d1e4094e0edc6e8be4bbe207c459f547d26
Caitlin Floyd <caitlinfloyd+coding@gmail.com>
2020-11-19 20:51:38 -0500

    style: adjust site colors

--- config/site.js
+++ config/site.js
@@ -14,8 +14,8 @@ module.exports = {
   author: "thundermiracle",
   trackingId: "UA-119418003-5",
   defaultLang: "en",
-  langTextMap: {
-    en: "English",
-    ja: "日本語",
-  },
+  // langTextMap: {
+  //   en: "English",
+  //   ja: "日本語",
+  // },
 };
