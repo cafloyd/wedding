3e91eddb6aec42fd916858407cd98d6361332993
Caitlin Floyd <caitlinfloyd+coding@gmail.com>
2020-11-21 00:01:46 -0500

    feat: reformat sections, including live stream and message

--- src/views/Sections/Services/Services.jsx
+++ src/views/Sections/Services/Services.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Row, Col } from "react-bootstrap";
-
-import ServiceItem from "components/ServiceItem";
+import { Row } from "react-bootstrap";
 import SectionHeader from "components/SectionHeader";
 import PageSection from "components/PageSection";
 
@@ -12,7 +10,7 @@ const Services = ({ className, frontmatter }) => {
     return null;
   }
 
-  const { anchor, header: rootHeader, subheader: rootSubHeader, services } = frontmatter;
+  const { anchor, header: rootHeader, subheader: rootSubHeader } = frontmatter;
 
   return (
     <PageSection className={className} id={anchor}>
@@ -20,11 +18,25 @@ const Services = ({ className, frontmatter }) => {
         <SectionHeader header={rootHeader} subheader={rootSubHeader} />
       </Row>
       <Row className="text-center">
-        {services.map((service) => (
-          <Col md={4} key={service.header}>
-            <ServiceItem {...service} />
-          </Col>
-        ))}
+        <div>
+          <p>
+            2020 has been a challenging year for everyone, but one good thing that has come out of it for us has been getting engaged. With so much unknown about the future, we didn&apos;t want to wait to get married and have decided to have a very small ceremony with immediate family this year. We wish we could have invited you to celebrate with us, but want to prioritize the health of our loved ones above anything else. With that in mind, we have decided to live stream the ceremony so that anyone interested can join us virtually.
+          </p>
+          <p>
+            The ceremony and live stream will be taking place on December 13 at 2pm EST, and the video will also be viewable afterwards. 
+          </p>
+          <p>
+            We hope you can join us, whether live for the virtual event or down the road when we have a belated reception to celebrate with you all.
+          </p>
+          {/* <br /> */}
+          <p>
+            Sending our love,
+          </p>
+          {/* <br /> */}
+          <p>
+            Caitlin & Justin
+          </p>
+        </div>
       </Row>
     </PageSection>
   );
