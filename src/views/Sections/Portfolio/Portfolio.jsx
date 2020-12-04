672b16b3600172003e6a8dd6e2601f2304c419af
Caitlin Floyd <caitlinfloyd+coding@gmail.com>
2020-12-03 20:50:51 -0500

    chore: fix countdown date

--- src/views/Sections/Portfolio/Portfolio.jsx
+++ src/views/Sections/Portfolio/Portfolio.jsx
@@ -223,8 +223,7 @@ const Portfolio = ({ className, frontmatter }) => {
       <div className="countdownAndVideoDiv">
         <Row>
           <Countdown
-            // date='2020-12-13T14:00:00-05:00'
-            date='2020-12-03T14:00:00-05:00'
+            date='2020-12-13T14:00:00-05:00'
             renderer={renderer}
           />
         </Row>
