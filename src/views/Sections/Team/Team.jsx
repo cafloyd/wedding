60f5e07868cc41ec1e7c9953abd10af77e798045
Caitlin Floyd <caitlinfloyd+coding@gmail.com>
2020-12-03 20:46:53 -0500

    feat: add giving section

--- src/views/Sections/Team/Team.jsx
+++ src/views/Sections/Team/Team.jsx
@@ -16,29 +16,31 @@ const Team = ({ className, frontmatter }) => {
     anchor,
     header: rootHeader,
     subheader: rootSubHeader,
-    content: rootContent,
     teamMember,
+    website
   } = frontmatter;
 
   return (
-    // <PageSection className={className} id={anchor}>
-    //   <Row>
-    //     <SectionHeader header={rootHeader} subheader={rootSubHeader} />
-    //   </Row>
-    //   <Row>
-    //     {teamMember.map(({ header, ...tmProps }) => (
-    //       <Col sm={4} key={header}>
-    //         <TeamMember header={header} {...tmProps} />
-    //       </Col>
-    //     ))}
-    //   </Row>
-    //   <Row>
-    //     <Col lg={8} className="mx-auto text-center">
-    //       <p className="large text-muted">{rootContent}</p>
-    //     </Col>
-    //   </Row>
-    // </PageSection>
-    <div />
+    <div>
+      <PageSection className={className} id={anchor}>
+        <Row>
+          <SectionHeader header={rootHeader} subheader={rootSubHeader} />
+        </Row>
+        <Row className="charities">
+          <Col lg={8} className="mx-auto text-center">
+            <p className="large text-muted">In lieu of a gift, we would love for you to treat yourself to a bottle of Champagne, or your drink of choice, to honor the start of our marriage. Feel free to send us a selfie, or use <b>#caitlinandjustin2020</b> on social media!</p>
+            <p className="large text-muted">If you are feeling moved by the spirit of the giving season, we would love for you to consider donating to <a href="https://nscphila.org/donate" target="_blank" rel="noreferrer">Nationalities Service Center</a> (where Caitlin used to work!), the <a href="https://secure.aspca.org/donate/donate-202011-mwdr1-t1-p1?ms=wb_top_homepage-donate&initialms=wb_top_homepage-donate&pcode=WEBMEMBER&lpcode=WEBGUARD" target="_blank" rel="noreferrer">American Society for the Prevention of Cruelty to Animals</a>, or <a href="https://dccentralkitchen.org/donate/" target="_blank" rel="noreferrer">DC Central Kitchen</a>.</p>
+          </Col>
+        </Row>
+        <Row>
+          {teamMember.map(({ header, ...tmProps }) => (
+            <Col sm={4} key={header}>
+              <TeamMember header={header} {...tmProps} />
+            </Col>
+          ))}
+        </Row>
+      </PageSection>
+    </ div>
   );
 };
 
